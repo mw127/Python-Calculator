cf0d5507ba1b55753a13d1f959d4b17b98675554
Markus Wulf <info@mawulf.de>
2021-11-18 20:06:28 +0100

    0.1.0 Addition

--- Taschenrechner.py
+++ Taschenrechner.py
@@ -1,3 +1,18 @@
 # Hier soll ein Programm entstehen, das die Funktion eines Taschenrechner haben soll
 
-print("Hello World")
+zahl1 = 0
+zahl2 = 0
+ergebnis =0
+
+print("Das Programm addiert zwei Zahlen")
+print("")
+
+zahl1 = int(input("Bitte die erste Zahl eingeben: "))
+zahl2 = int(input("Nun die zweite Zahl eingeben: "))
+print("Die Summe der beiden Zahlen ist: " + str(zahl1 + zahl2))
+
+
+#ergebnis=zahl1+zahl2
+#print("Die Summe der beiden Zahlen ist:" + ergebnis)    #auch eine Möglichkeit
+
+print("Auf Wiedersehen")
